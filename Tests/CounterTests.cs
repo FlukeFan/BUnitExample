63c276ccc27a5b52743db85dfafc706ea8b063e3
Richard Brown <fluke_fan@hotmail.com>
2022-04-23 10:38:41 +0100

    Added failing example.

---
+++ Tests/CounterTests.cs
@@ -0,0 +1,23 @@
+﻿using Bunit;
+using NUnit.Framework;
+using Site.Pages;
+
+namespace Tests
+{
+    public class CounterTests
+    {
+        [Test]
+        public void TestCounter()
+        {
+            using var bunit = new Bunit.TestContext();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var cut1 = bunit.RenderComponent<Counter>();
+                cut1.WaitForAssertion(() => cut1.Find("[data-id=1]"));
+                cut1.Find("[data-id=1]").Click();
+                cut1.WaitForAssertion(() => cut1.Find("[data-id=2]"));
+            }
+        }
+    }
+}
